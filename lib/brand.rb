f3acad8868b1af83f7de322081ae5640ca3821bd
Maldon <maldonmeehan@gmail.com>
2016-07-22 07:16:32 -0700

    add migrations for shoe_devlopment database and tables for brands, stores and brands_stores

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,1 +1,10 @@
 require('pry')
+
+class Brand < ActiveRecord::Base
+  has_and_belongs_to_many (:recipes)
+  # validates(:name, {:presence => true, :length => {:maximum => 50 }})
+  #
+  # scope(:not_done, -> do
+  #   where({:done => false})
+  # end)
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,1 +1,10 @@
 require('pry')
+
+class Store < ActiveRecord::Base
+  has_and_belongs_to_many(:brands)
+  # validates(:name, {:presence => true, :length => {:maximum => 50 }})
+  #
+  # scope(:not_done, -> do
+  #   where({:done => false})
+  # end)
+end
