6f090990be16934666ee4a962b4e7a68993f4f5e
Maldon <maldonmeehan@gmail.com>
2016-07-22 09:19:11 -0700

    add passing intergration spec for add a new store

--- app.rb
+++ app.rb
@@ -10,3 +10,19 @@ end
 get('/stores/new') do
   erb(:store_form)
 end
+
+get('/stores') do
+  @stores = Store.all()
+  erb(:stores)
+end
+
+post('/stores') do
+  name = params.fetch("name")
+  @store = Store.new({:name => name, :id => nil})
+  @stores = Store.all()
+  if @store.save()
+    erb(:stores)
+  else
+    erb(:errors)
+  end
+end
--- spec/intergraation_spec.rb
+++ spec/intergraation_spec.rb
@@ -14,3 +14,13 @@ describe('the add new store route', {:type => :feature}) do
     expect(page).to have_content('Add new store:')
   end
 end
+
+describe('add a new store', {:type => :feature}) do
+  it('allows the user to add a store') do
+    visit('/')
+    click_link('Add new store')
+    fill_in("name", :with => 'Shoe Mill')
+    click_button('Add store')
+    expect(page).to have_content('Shoe Mill')
+  end
+end
