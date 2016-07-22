7608d9ceabee79bd5c802ab378b88a1fe62a3913
Maldon <maldonmeehan@gmail.com>
2016-07-22 10:53:10 -0700

    refactor views

--- app.rb
+++ app.rb
@@ -8,27 +8,27 @@ get ('/') do
   erb(:index)
 end
 
-get('/stores/new') do
-  erb(:store_form)
-end
-
-get('/stores') do
-  @stores = Store.all()
-  erb(:stores)
-end
-
-post('/stores') do
-  name = params.fetch("name")
-  @store = Store.new({:name => name, :id => nil})
-  @stores = Store.all()
-  if @store.save()
-    erb(:stores)
-  else
-    erb(:errors)
-  end
-end
-
-get('/stores/:id') do
-  @store = Store.find(params.fetch("id").to_i())
-  erb(:store)
-end
+# get('/stores/new') do
+#   erb(:store_form)
+# end
+#
+# get('/stores') do
+#   @stores = Store.all()
+#   erb(:stores)
+# end
+#
+# post('/stores') do
+#   name = params.fetch("name")
+#   @store = Store.new({:name => name, :id => nil})
+#   @stores = Store.all()
+#   if @store.save()
+#     erb(:stores)
+#   else
+#     erb(:errors)
+#   end
+# end
+#
+# get('/stores/:id') do
+#   @store = Store.find(params.fetch("id").to_i())
+#   erb(:store)
+# end
--- spec/intergraation_spec.rb
+++ spec/intergraation_spec.rb
@@ -7,30 +7,30 @@ describe('the home page route', :type => :feature) do
   end
 end
 
-describe('the add new store page route', {:type => :feature}) do
-  it('takes the user to the store form page') do
-    visit('/')
-    click_link('Add new store')
-    expect(page).to have_content('Add new store:')
-  end
-end
-
-describe('add a new store', {:type => :feature}) do
-  it('allows the user to add a store') do
-    visit('/')
-    click_link('Add new store')
-    fill_in("name", :with => 'Shoe Mill')
-    click_button('Add store')
-    expect(page).to have_content('Shoe Mill')
-  end
-end
-
-describe('the store errors route', {:type => :feature}) do
-  it('takes the user to the errors page') do
-    visit('/')
-    click_link('Add new store')
-    fill_in("name", :with => '')
-    click_button('Add store')
-    expect(page).to have_content('Oops there were some errors:')
-  end
-end
+# describe('the add new store page route', {:type => :feature}) do
+#   it('takes the user to the store form page') do
+#     visit('/')
+#     click_link('Add new store')
+#     expect(page).to have_content('Add new store:')
+#   end
+# end
+#
+# describe('add a new store', {:type => :feature}) do
+#   it('allows the user to add a store') do
+#     visit('/')
+#     click_link('Add new store')
+#     fill_in('name', :with => 'Shoe Mill')
+#     click_button('Add store')
+#     expect(page).to have_content('Shoe Mill')
+#   end
+# end
+#
+# describe('the store errors route', {:type => :feature}) do
+#   it('takes the user to the errors page') do
+#     visit('/')
+#     click_link('Add new store')
+#     fill_in('name', :with => '')
+#     click_button('Add store')
+#     expect(page).to have_content('Oops there were some errors:')
+#   end
+# end
