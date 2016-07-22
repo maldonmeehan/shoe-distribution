7c88b04ee258c13968ebe01171bcf7625b4dccee
Maldon <maldonmeehan@gmail.com>
2016-07-22 08:14:58 -0700

    add passing store spec for belongs to many brands and fix formating in brand_spec

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -11,6 +11,16 @@ describe(Store) do
     expect(store.name()).to(eq('Shoe Mill'))
   end
 
+  describe("#brands") do
+    it('belongs to many brands') do
+      test_store = Store.create({:name => 'Shoe Mill'})
+      test_brand1 = Brand.create({:name => 'Nike'})
+      test_brand2 = Brand.create({:name => 'Converse'})
+      test_brand1.stores.push(test_store)
+      test_brand2.stores.push(test_store)
+      expect(test_store.brands()).to(eq([test_brand1, test_brand2]))
+    end
+  end
 
 
 end
