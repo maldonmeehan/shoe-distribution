5618afbb2c3a5867e58a372b60d835cb582178d6
Maldon <maldonmeehan@gmail.com>
2016-07-22 12:36:21 -0700

    update content on store page

--- spec/store_intergraation_spec.rb
+++ spec/store_intergraation_spec.rb
@@ -35,6 +35,18 @@ describe('the store errors route', {:type => :feature}) do
   end
 end
 
+# describe('view single store instance path', {:type => :feature}) do
+#   it('allows the user to view a single store') do
+#     visit('/')
+#     click_link('Add new store')
+#     fill_in("name", :with => 'Payless Shoes')
+#     click_button('Add store')
+#     expect(page).to have_content('Payless Store')
+#     click_link("Payless Store")
+#     expect(page).to have_content('Individual store page')
+#   end
+# end
+
 describe('update a store name route', {:type => :feature}) do
   it('allows the user to update the store name') do
     visit('/')
