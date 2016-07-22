aeb26f3189df3f7f8339be86bb336e8097e42fda
Maldon <maldonmeehan@gmail.com>
2016-07-22 08:57:38 -0700

    add failing intergration spec for add a new store

--- spec/intergraation_spec.rb
+++ spec/intergraation_spec.rb
@@ -6,3 +6,13 @@ describe('the home page route', :type => :feature) do
     expect(page).to have_content('Welcome')
   end
 end
+
+describe('add a new store', {:type => :feature}) do
+  it('allows the user to add a store') do
+    visit('/')
+    click_link('Add new store')
+    fill_in('name', :with => 'Shoe Mill')
+    click_button('Add store')
+    expect(page).to have_content('Shoe Mill')
+  end
+end
