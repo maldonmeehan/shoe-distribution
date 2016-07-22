4f4b74911e005a63ddc397a812b007f8d6e290cc
Maldon <maldonmeehan@gmail.com>
2016-07-22 09:35:23 -0700

    add failing intergration spec for the add new brand page route

--- spec/intergraation_spec.rb
+++ spec/intergraation_spec.rb
@@ -7,21 +7,11 @@ describe('the home page route', :type => :feature) do
   end
 end
 
-describe('the add new store route', {:type => :feature}) do
+describe('the add new store page route', {:type => :feature}) do
   it('takes the user to the store form page') do
     visit('/')
     click_link('Add new store')
     expect(page).to have_content('Add new store:')
-  end
-end
-
-describe('add a new store', {:type => :feature}) do
-  it('allows the user to add a store') do
-    visit('/')
-    click_link('Add new store')
-    fill_in("name", :with => 'Shoe Mill')
-    click_button('Add store')
-    expect(page).to have_content('Shoe Mill')
   end
 end
 
@@ -42,5 +32,13 @@ describe('the store errors route', {:type => :feature}) do
     fill_in("name", :with => '')
     click_button('Add store')
     expect(page).to have_content('Oops there were some errors:')
+  end
+end
+
+describe('the add new brand page route', {:type => :feature}) do
+  it('takes the user to the brand form page') do
+    visit('/')
+    click_link('Add new brand')
+    expect(page).to have_content('Add new brand:')
   end
 end
