2418d977fa11bae9ed50be205f90f4e7a759529a
Maldon <maldonmeehan@gmail.com>
2016-07-22 07:21:51 -0700

    add passing brand spec for validates presence of a brand

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,10 +1,12 @@
 require('pry')
 
+# brand is ingredience
+
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many (:recipes)
-  # validates(:name, {:presence => true, :length => {:maximum => 50 }})
-  #
-  # scope(:not_done, -> do
-  #   where({:done => false})
-  # end)
+  validates(:name, {:presence => true, :length => {:maximum => 50 }})
+
+  scope(:not_done, -> do
+    where({:done => false})
+  end)
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,10 +1,12 @@
 require('pry')
 
+# store is recipe
+
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
-  # validates(:name, {:presence => true, :length => {:maximum => 50 }})
-  #
-  # scope(:not_done, -> do
-  #   where({:done => false})
-  # end)
+  validates(:name, {:presence => true, :length => {:maximum => 50 }})
+
+  scope(:not_done, -> do
+    where({:done => false})
+  end)
 end
