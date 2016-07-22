572375d35033f86ae0133d78f62642feab69d8f7
Maldon <maldonmeehan@gmail.com>
2016-07-22 09:28:58 -0700

    add passing spec for the store errors route

--- spec/intergraation_spec.rb
+++ spec/intergraation_spec.rb
@@ -24,3 +24,23 @@ describe('add a new store', {:type => :feature}) do
     expect(page).to have_content('Shoe Mill')
   end
 end
+
+describe('add a new store', {:type => :feature}) do
+  it('allows the user to add a store') do
+    visit('/')
+    click_link('Add new store')
+    fill_in("name", :with => 'Shoe Mill')
+    click_button('Add store')
+    expect(page).to have_content('Shoe Mill')
+  end
+end
+
+describe('the store errors route', {:type => :feature}) do
+  it('takes the user to the errors page') do
+    visit('/')
+    click_link('Add new store')
+    fill_in("name", :with => '')
+    click_button('Add store')
+    expect(page).to have_content('Oops there were some errors:')
+  end
+end
