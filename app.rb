f5ab0212931a58d060b4ab71fdef46e4af739572
Maldon <maldonmeehan@gmail.com>
2016-07-22 11:50:07 -0700

    revert back to orginal views plan

--- app.rb
+++ app.rb
@@ -9,26 +9,41 @@ get ('/') do
 end
 
 get('/stores/new') do
+  erb(:store_form)
+end
+
+get('/stores') do
+  @stores = Store.all()
+  erb(:stores)
+end
+
+# post ('/stores/new') do
+#   name = params.fetch("name")
+#   @new_store = Store.new({:name => name})
+#   if @new_store.save()
+#     redirect("/stores/".concat(@new_store.id().to_s()))
+#   else
+#     erb(:index)
+#   end
+# end
+
+get('/stores/:id') do
+  @store = Store.find(params.fetch("id").to_i())
   erb(:form)
 end
 
-# get('/stores') do
-#   @stores = Store.all()
-#   erb(:stores)
-# end
-#
-# post('/stores') do
-#   name = params.fetch("name")
-#   @store = Store.new({:name => name, :id => nil})
-#   @stores = Store.all()
-#   if @store.save()
-#     erb(:stores)
-#   else
-#     erb(:errors)
-#   end
-# end
-#
-# get('/stores/:id') do
-#   @store = Store.find(params.fetch("id").to_i())
-#   erb(:store)
-# end
+post('/stores') do
+  name = params.fetch("name")
+  @store = Store.new({:name => name, :id => nil})
+  @stores = Store.all()
+  if @store.save()
+    erb(:stores)
+  else
+    erb(:errors)
+  end
+end
+
+get('/stores/:id') do
+  @store = Store.find(params.fetch("id").to_i())
+  erb(:store)
+end
